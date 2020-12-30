54f856136987b8fd927124cb0df224682ffba5cd
bfan05 <76703988+bfan05@users.noreply.github.com>
2020-12-30 15:30:08 -0800

    Update lightson.cpp

--- Silver/Dec2015/lightson.cpp
+++ Silver/Dec2015/lightson.cpp
@@ -6,96 +6,78 @@ using ll = long long;
 
 using vi = vector<int>;
 #define pb push_back
-#define rsz resize
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rep(i, a, b) for(ll i = a; i < b; ++i)
+#define rsz(x, n) x.resize(n)
 
 using pi = pair<int, int>;
 #define f first
 #define s second
-#define mp make_pair
 
 void setIO(string name = "lightson") {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    if (sz(name)) {
+    if (name.size()) {
         freopen((name + ".in").c_str(), "r", stdin);
         freopen((name + ".out").c_str(), "w", stdout);
     }
 }
 
-int main() {
+ll n, m;
+map<pair<ll, ll>, vector<pair<ll, ll>>> mp;
+vector<vector<ll>> lightson;
+vector<vector<bool>> visited;
+bool fnew = true;
+
+void dfs(ll i, ll j) {
+    if (i < 0 || i >= n || j < 0 || j >= n || !lightson[i][j] || visited[i][j])
+        return;
+    visited[i][j] = true;
+    for (pair<ll, ll> &cd : mp[{ i, j }]) {
+        if (!lightson[cd.first][cd.second]) {
+            fnew = true;
+            lightson[cd.first][cd.second] = 1;
+        }
+    }
+    ll dir[] = { 0, 1, 0, -1, 0 };
+    rep(k, 0, 4) {
+        ll dx = i + dir[k];
+        ll dy = j + dir[k + 1];
+        dfs(dx, dy);
+    }
+}
+
+int main()
+{
     setIO();
-    int n, m;
     cin >> n >> m;
-    vector<vector<int>> edges;
-    vector<int> rooms;
-    for (int i = 0; i < n * n; ++i) {
-        rooms.pb(0);
+    rsz(lightson, n);
+    rsz(visited, n);
+    rep(i, 0, m) {
+        ll a, b, c, d; cin >> a >> b >> c >> d;
+        --a; --b; --c; --d;
+        mp[{a, b}].push_back({ c, d });
     }
-    rooms[0] = 1;
-    int count = 0;
-    for (int i = 0; i < n * n; ++i) {
-        vector<int> edge;
-        if (i / n > 0) {
-            edge.pb(i - n);
-        }
-        if ((i / n) < (n - 1)) {
-            edge.pb(i + n);
-        }
-        if (i % n > 0) {
-            edge.pb(i - 1);
-        }
-        if (i % n < n - 1) {
-            edge.pb(i + 1);
-        }
-        edges.pb(edge);
-    }
-    vector<vector<int>> lights;
-    for (int i = 0; i < n * n; ++i) {
-        lights.pb({});
-    }
-    for (int i = 0; i < m; ++i) {
-        int x, y, a, b;
-        cin >> x >> y >> a >> b;
-        x--; y--; a--; b--;
-        lights[x * n + y].pb(a * n + b);
-    }
-    bool foundNew = true;
-    int c = -1;
-    while (foundNew) {
-        int nc = 0;
-        queue<int> breadth;
-        breadth.push(0);
-        vector<bool> visited;
-        for (int i = 0; i < n * n; ++i) {
-            visited.pb(false);
-        }
-        while (!breadth.empty()) {
-            ++nc;
-            visited[breadth.front()] = true;
-            for (int i = 0; i < lights[breadth.front()].size(); ++i) {
-                rooms[lights[breadth.front()][i]] = 1;
-            }
-            for (int i = 0; i < edges[breadth.front()].size(); ++i) {
-                if (rooms[edges[breadth.front()][i]] == 1 && !visited[edges[breadth.front()][i]]) {
-                    breadth.push(edges[breadth.front()][i]);
-                    visited[edges[breadth.front()][i]] = true;
-                }
-            }
-            breadth.pop();
-        }
-        if (nc != c) {
-            foundNew = true;
-            c = nc;
-        }
-        else {
-            foundNew = false;
+    rep(i, 0, n) {
+        rep(j, 0, n) {
+            lightson[i].push_back(0);
+            visited[i].push_back(false);
         }
     }
-    for (int i = 0; i < n * n; ++i) {
-        if (rooms[i] == 1) {
-            ++count;
+    lightson[0][0] = 1;
+    while (fnew) {
+        fnew = false;
+        visited.clear();
+        rsz(visited, n);
+        rep(i, 0, n) {
+            rep(j, 0, n)
+                visited[i].push_back(false);
         }
+        dfs(0, 0);
     }
-    cout << count << endl;
+    ll ans = 0;
+    rep(i, 0, n) {
+        rep(j, 0, n)
+            if (lightson[i][j]) ++ans;
+    }
+    cout << ans << endl;
 }
